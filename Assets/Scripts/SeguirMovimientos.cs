2f6f0291c3ed62c09bd2ea7a03a74fd70ca3f331
Carlos Gomez <carlosyoshy10@gmail.com>
2024-05-13 18:25:31 -0600

    Escena antes de juego
    :video_game: :video_game: :video_game: :video_game: :video_game: :video_game: :video_game:

---
+++ Assets/Scripts/SeguirMovimientos.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguirMovimientos : MonoBehaviour
+{
+    public GameObject[] targetObject1; 
+    public GameObject[] targetObject2;
+
+    public GameObject[] objectToMove;
+
+    public GameObject[] side1;
+    public GameObject[] side2;
+
+    private bool isTarget1Active = true;
+    public void ChangeSide()
+    {
+        if (objectToMove != null && targetObject1 != null && targetObject2 != null)
+        {
+            if (isTarget1Active)
+            {
+                for (int i = 0; i < objectToMove.Length; i++)
+                {
+                    objectToMove[i].transform.position = targetObject1[i].transform.position;
+                }
+
+            }
+            else
+            {
+                for (int i = 0; i < objectToMove.Length; i++)
+                {
+                    objectToMove[i].transform.position = targetObject2[i].transform.position;
+                }
+            }
+        }
+    }
+    public void ChangeHand()
+    {
+        if (objectToMove != null && targetObject1 != null && targetObject2 != null)
+        {
+            if (isTarget1Active)
+            {
+                foreach (GameObject obj in side2)
+                {
+                    obj.SetActive(false);
+                }
+                foreach (GameObject obj in side1)
+                {
+                    obj.SetActive(true);
+                }
+            }
+            else
+            {
+                foreach (GameObject obj in side1)
+                {
+                    obj.SetActive(false);
+                }
+                foreach (GameObject obj in side2)
+                {
+                    obj.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void ToggleTargets(bool side)
+    {
+        isTarget1Active = side;
+        ChangeSide();
+        ChangeHand();
+    }
+}
